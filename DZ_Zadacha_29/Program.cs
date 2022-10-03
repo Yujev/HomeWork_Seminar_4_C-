f97f44359963ff4623145b926e9b0bff0f4ddd86
Yujev <ustatar@ya.ru>
2022-10-03 17:29:04 +0300

    Решил задачу 29

---
+++ DZ_Zadacha_29/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+int[] massive = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(", ", massive)}]");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(8);
+    }
+    return result;
+
+}
